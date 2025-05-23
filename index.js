a87d844287af5757d134636c11c52d6b51767c13
saidovanvar <saidovanvar@gmail.com>
2025-05-23 14:20:37 +0500

    js3

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-export let input = prompt("Yoshni kiriting");
-export function validator(a) {
+const form = document.querySelector('#form');
+const input = document.querySelector('#username');
+const result = document.querySelector('#result');
 
-    if (a < 0 || a > 120) {
-        throw new Error('Agar yosh 0 dan kichik yoki 120dan katta');
-    }
+let names = JSON.parse(localStorage.getItem('names')) || [];
+
+function main() {
+    result.innerHTML = '';
+    names.forEach(name => {
+        const div = document.createElement('div');
+        div.textContent = name;
+        result.appendChild(div);
+    })
+
 }
+
+main();
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = input.value.trim();
+    if (name) {
+        names.push(name);
+        localStorage.setItem('names', JSON.stringify(names));
+        main();
+        input.value = '';
+    }
+});
